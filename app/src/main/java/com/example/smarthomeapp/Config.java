29ca2bb0cfc85587d001699f3c8cd0d920d152c0
America <americarivera49@gmail.com>
2022-03-13 11:31:59 -0600

    Completado

--- app/src/main/java/com/example/smarthomeapp/Config.java
+++ app/src/main/java/com/example/smarthomeapp/Config.java
@@ -1,6 +1,6 @@
 package com.example.smarthomeapp;
 
 public class Config {
-    public static final String URL = "http://172.17.123.97/smarthome/";
+    public static final String URL = "http://192.168.1.77/smarthome/";
     //192.168.1.72 172.17.123.97
 }
--- app/src/main/java/com/example/smarthomeapp/MainActivity.java
+++ app/src/main/java/com/example/smarthomeapp/MainActivity.java
@@ -6,15 +6,18 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
@@ -53,29 +56,25 @@ public class MainActivity extends AppCompatActivity {
                 }, new Response.ErrorListener(){
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "Error " +url, Toast.LENGTH_SHORT).show();
-               // etUser.setText(url);
+                Toast.makeText(MainActivity.this, "Error"+error.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("Server", error.getMessage());
             }
         });
         MySingleton.getInstance(getApplicationContext()).addToRequestQueue(peticion);
     }
 
-
-
     private void respuesta(JSONObject response){
         try {
-            if (response.getString("login").compareTo("y")== 0){
+            if(response.getString("login").compareTo("y")==0){
                 String jwt = response.getString("token");
                 SharedPreferences.Editor editor = sesion.edit();
-                editor.putString("user", etUser.getText().toString());
+                editor.putString("user",etUser.getText().toString());
                 editor.putString("token", jwt);
                 editor.commit();
-                startActivity( new Intent (this, MainActivity2.class));
-            }else {
+                startActivity(new Intent(this, MainActivity2.class));
+            }else{
                 Toast.makeText(this, "Error de usuario o contraseña", Toast.LENGTH_SHORT).show();
             }
         }catch (Exception e){}
     }
-
 }
-
